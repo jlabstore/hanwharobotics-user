1ecc1e7a9f3bd6bcf08fa7c5a513c0da11cca7f2
eunhachoi <eunha1040@jlabsoft.com>
2023-11-22 16:20:59 +0900

    fix: customerror 제거

--- src/main/java/com/hanwha/robotics/user/controller/CustomErrorController.java
+++ src/main/java/com/hanwha/robotics/user/controller/CustomErrorController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
     
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request)  {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    // @RequestMapping("/error")
+    // public String handleError(HttpServletRequest request)  {
+    //     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-        if(status != null && Integer.valueOf(status.toString()) ==  HttpStatus.NOT_FOUND.value()){
-            return "redirect:/";
-        }
+    //     if(status != null && Integer.valueOf(status.toString()) ==  HttpStatus.NOT_FOUND.value()){
+    //         return "redirect:/";
+    //     }
 
-        return "error/error";
-    }
+    //     return "error/error";
+    // }
 }
