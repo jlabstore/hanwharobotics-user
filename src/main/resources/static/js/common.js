9cae021cede57f8d0420d16e0b906e042e526468
dljuhee <keunhyeong@jlabsoft.com>
2023-11-27 14:03:27 +0900

    design: 231127 퍼블 적용

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,9 +1,26 @@
+const win = $(this); //this = window
+
 // pc, table, mobile 여부
 $(window).on("load resize", function (e) {
-  const win = $(this); //this = window
+  // const win = $(this); //this = window
+
+  let top = 0;
+  let headerHeight = '332px';
+
+  if (win.width() <= 1023) {
+    top = 70;
+  } else if (win.width() > 1023 && win.width() <= 1190) {
+    top = 167;
+  } else {
+    top = 207;
+  }
+
+  if(win.width() <= 1550) {
+    headerHeight = '450px';
+  }
 
   $(document).on('mouseover', '#header .header_inner', function() {
-    $(this).closest('#header').css({'height': '332px'});
+    $(this).closest('#header').css({'height': headerHeight});
     $(this).find('.header__nav__items_sub').addClass('active');
     $(this).closest('#header').addClass('active');
   })
@@ -14,26 +31,46 @@ $(window).on("load resize", function (e) {
   });
 
 
-  if (win.width() <= 1325) {
+  if (win.width() <= 1024) {
     $(document).off('mouseover mouseleave');
     $('header').removeClass('active');
     $('header .gnb_layer').removeClass('active');
     $('header .gnb_menu').removeClass('active');
+
+
+    $(document).on('click', '#header .header_inner .gnb_menu', function(e) {
+      e.stopPropagation();
+
+      if(e.target.tagName !== 'A') {
+        $(this).find('.header__nav__items_sub').addClass('active');
+        $(this).closest('#header').css({'height': headerHeight}).addClass('active');
+        // $(this).closest('.header__nav__items_sub').addClass('active');
+      }
+    });
   }
 
   if (win.width() <= 1024) {
     $("body").attr("class", "mobile");
-  } else if (win.width() < 1500) {
+  } else if (win.width() < 1550) {
     $("body").attr("class", "tablet");
   } else {
     $("body").attr("class", "pc");
   }
   $("header").css('visibility', 'visible');
-});
 
+    setTimeout(function() {
+      // more_btn 요소를 선택합니다.
+      const moreBtn = $('#aside .more_btn');
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+      // Convert the current top style to an integer and add the scroll position
+      // const currentTop = parseInt(moreBtn.css('top'), 10);
+      moreBtn.css('top', top + scrollPosition + 'px');
+    }, 300);
+  });
 
 $(document).ready(async function() {
   $('#header').load('/includes/header.html');
+  $('#aside').load('../includes/aside.html');
   $('#footer').load('/includes/footer.html', function() {
     changeUrl();
   });
@@ -54,21 +91,42 @@ $(document).ready(async function() {
   $(document).on('click', 'header .gnb_layer, header .gnb_menu .btn_close', function(e) {
     e.stopPropagation();
 
+    $('#header .header__nav__items_sub').removeClass('active');
+    $(this).closest('#header').removeClass('active');
     $(this).removeClass('active');
     $('header .btn_menu button').removeClass('active');
     $('.gnb_menu, .gnb_layer').removeClass('active');
+    $('#header').css({'height': 'auto'});
     $('html').removeClass('scroll-lock');
   });
 
-  // $(document).on('click', 'header button.inquiry', function() {
-  //   $('body').addClass('scroll-lock');
+  $(document).on('click', 'header .gnb_menu .header__nav__items .header__nav__items_sub a', function(e) {
+    e.stopPropagation();
+
+    $('#header .header__nav__items_sub').removeClass('active');
+    $(this).closest('#header').removeClass('active');
+    $(this).removeClass('active');
+    $('header .btn_menu button').removeClass('active');
+    $('.gnb_menu, .gnb_layer').removeClass('active');
+    $('#header').css({'height': 'auto'});
+    $('html').removeClass('scroll-lock');
+  });
+
+  // $(document).on('click', '#header button.inquiry, #aside button.inquiry', function(e) {
+  //   e.stopPropagation();
+
+  //   // $('body').addClass('scroll-lock');
   //   $('.layer.inquiry').show();
   //   $('.layer_bg').show();
-
   //   $(this).removeClass('active');
+  //   $('html').removeClass('scroll-lock');
+
   //   $('header .btn_menu button').removeClass('active');
   //   $('.gnb_menu, .gnb_layer').removeClass('active');
-  //   $('html').removeClass('scroll-lock'); 
+
+  //   $('.header__nav__items_sub').removeClass('active');
+  //   $('#header').removeClass('active');
+  //   $('#header').css({'height': 'auto'});
   // });
 
   // $(document).on('click', '.layer_bg, .layer .btn_close', function() {
@@ -77,6 +135,29 @@ $(document).ready(async function() {
   //   $('.layer_bg').hide();
   // });
 
-
 });
-  
+
+// 스크롤 이벤트 리스너를 추가합니다.
+window.addEventListener('scroll', throttle(function() {
+  requestAnimationFrame(() => {
+    let top = 0;
+
+    if (win.width() <= 1024) {
+      top = 70;
+    } else if (win.width() > 1024 && win.width() <= 1190) {
+      top = 167;
+    } else {
+      top = 207;
+    }
+
+    // more_btn 요소를 선택합니다.
+    const moreBtn = $('#aside .more_btn');
+
+    // 브라우저 창 상단으로부터의 현재 스크롤 위치를 얻습니다.
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Convert the current top style to an integer and add the scroll position
+    // const currentTop = parseInt(moreBtn.css('top'), 10);
+    moreBtn.css('top', top + scrollPosition + 'px');
+  });
+},3));
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -46,76 +46,76 @@ $(window).on("resize", function () {
   const $missionBox = $('.section2 .second_box');
   hideHeader();
 
-  if ($(this).width() >= 1190) {
-    pcBoxPositionUp();
-  } else {
+  // if ($(this).width() >= 1190) {
+  //   pcBoxPositionUp();
+  // } else {
   
-  $missionBox.css('top', '0')
-  .off('mouseenter mouseleave');
-  }
+  //   $missionBox.css('top', '0')
+  //   .off('mouseenter mouseleave');
+  // }
 });
 
 $(window).on("load", function (e) {
   const $missionBox = $('.section2 .second_box');
   hideHeader();
 
-  if ($(this).width() >= 1190) {
-    pcBoxPositionUp();
-  } else {
-    $missionBox.css('top', '0')
-    .off('mouseenter mouseleave');
-  }
+  // if ($(this).width() >= 1190) {
+  //   pcBoxPositionUp();
+  // } else {
+  //   $missionBox.css('top', '0')
+  //   .off('mouseenter mouseleave');
+  // }
 
-  document.querySelectorAll('.main .section1 .contents').forEach((section, index, array) => {
+  // document.querySelectorAll('.main .section1 .contents').forEach((section, index, array) => {
   
-    // Pin the section
-    gsap.to(section, {
-      scrollTrigger: {
-        trigger: section,
-        start: "top top",
-        end: "bottom top",
-        pin: true,
-        scrub: true
-      }
-    });
+  //   // Pin the section
+  //   gsap.to(section, {
+  //     scrollTrigger: {
+  //       trigger: section,
+  //       start: "top top",
+  //       end: "bottom top",
+  //       pin: true,
+  //       scrub: true
+  //     }
+  //   });
     
-    // Only proceed if there is a next section
-    if (index < array.length - 1) {
-      const nextSection = array[index + 1]; // Get the next section
+  //   // Only proceed if there is a next section
+  //   if (index < array.length - 1) {
+  //     const nextSection = array[index + 1]; // Get the next section
 
-      const textElements = section.querySelectorAll('.title, .txt, .move_to_intro');
-      const nextTextElements = nextSection.querySelectorAll('.title, .txt, .move_to_intro');
+  //     const textElements = section.querySelectorAll('.title, .txt, .move_to_intro');
+  //     const nextTextElements = nextSection.querySelectorAll('.title, .txt, .move_to_intro');
 
 
-      // Create a timeline for the current section's text elements
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: nextSection, // Use the next section as the trigger
-          start: "top bottom", // Start when the top of the next section hits the bottom of the viewport
-          end: "top top", // End when the top of the next section hits the top of the viewport
-          scrub: true,
-          onEnter: () => {
-            // When the next section's .title and .txt start coming in, fade out the current section's .title and .txt
-            textElements.forEach(el => {
-              gsap.to(el, { opacity: 0, duration: 3 });
-            });           
+  //     // Create a timeline for the current section's text elements
+  //     const tl = gsap.timeline({
+  //       scrollTrigger: {
+  //         trigger: nextSection, // Use the next section as the trigger
+  //         start: "top bottom", // Start when the top of the next section hits the bottom of the viewport
+  //         end: "top top", // End when the top of the next section hits the top of the viewport
+  //         scrub: true,
+  //         onEnter: () => {
+  //           // When the next section's .title and .txt start coming in, fade out the current section's .title and .txt
+  //           textElements.forEach(el => {
+  //             gsap.to(el, { opacity: 0, duration: 3 });
+  //           });           
             
-          },
-          onLeaveBack: () => {
-            // When scrolling back up, reset the opacity if the previous .title and .txt are in view again
-            textElements.forEach(el => {
-              gsap.to(el, { opacity: 1, duration: 3 });
-            });
-          }
-        }
-      });
+  //         },
+  //         onLeaveBack: () => {
+  //           // When scrolling back up, reset the opacity if the previous .title and .txt are in view again
+  //           textElements.forEach(el => {
+  //             gsap.to(el, { opacity: 1, duration: 3 });
+  //           });
+  //         }
+  //       }
+  //     });
 
-      // Fade in the next section's text elements gradually
-      nextTextElements.forEach(el => {
-        tl.fromTo(el, { opacity: 0 }, { opacity: 1, duration: 3, ease: "none" });
-      });
-    }
-  });
+  //     // Fade in the next section's text elements gradually
+  //     nextTextElements.forEach(el => {
+  //       tl.fromTo(el, { opacity: 0 }, { opacity: 1, duration: 3, ease: "none" });
+  //     });
+  //   }
+  // });
   
   
   
