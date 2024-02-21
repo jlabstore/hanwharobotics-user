f7032c36e3cb583ac6054643a3946ada43699848
dljuhee <dewi@jlabsoft.com>
2024-02-21 16:50:22 +0900

    design: 메인 페이지 이미지 변경, 경영방침 페이지 추가

--- src/main/java/com/hanwha/robotics/user/controller/AboutController.java
+++ src/main/java/com/hanwha/robotics/user/controller/AboutController.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/about")
 public class AboutController {
-    
+
     /**
-     * about 페이지 
+     * about 페이지
      * @return
      */
     @GetMapping("")
     public String about(){
         return "about/about";
+    }
+
+    /**
+     * 경영방침 페이지
+     */
+    @GetMapping("/managementPolicy")
+    public String managementPolicy() {
+        return "about/management_policy";
     }
 }
