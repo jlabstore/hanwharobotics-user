2edd562e20e0a21765c5091e49b38e7fa0b82d69
dljuhee <dewi@jlabsoft.com>
2024-02-21 19:21:59 +0900

    design: 한화로보틱스 페이지 수정

--- src/main/java/com/hanwha/robotics/user/controller/AboutController.java
+++ src/main/java/com/hanwha/robotics/user/controller/AboutController.java
@@ -2,7 +2,9 @@ package com.hanwha.robotics.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/about")
@@ -13,9 +15,23 @@ public class AboutController {
      * @return
      */
     @GetMapping("")
-    public String about(){
-        return "about/about";
+    public String about(@RequestParam String lang){
+        if ("en".equals(lang)) {
+            // 영문페이지
+            return "about/about";
+		} else {
+            // 국문페이지
+            return "about/aboutKr";
+		}
     }
+
+    // /**
+    //  * about 임시 한글 페이지
+    //  */
+    // @GetMapping("/about")
+    // public String aboutKr(){
+    //     return "about/aboutKr";
+    // }
 
     /**
      * 경영방침 페이지
