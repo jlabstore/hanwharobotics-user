d249c4408a37ce3891b72bf01389cc5f7b7e1ace
dljuhee <dewi@jlabsoft.com>
2024-03-25 14:38:09 +0900

    design: 솔루션 페이지 생성

--- src/main/java/com/hanwha/robotics/user/controller/RobotController.java
+++ src/main/java/com/hanwha/robotics/user/controller/RobotController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/robot")
 public class RobotController {
@@ -17,6 +18,12 @@ public class RobotController {
     public String robotDetail() {
         return "robot/robots_detail_hcr_5a";
     }
+
+    @GetMapping("/solutions")
+    public String solutions() {
+        return "robot/solutions";
+    }
+
 
 
 }
