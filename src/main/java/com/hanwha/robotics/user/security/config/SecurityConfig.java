7d030d6103908fe8f73eaa492c9243af4b8bc0b1
hyunjeong <hyunjeong@jlabsoft.com>
2024-04-05 16:31:29 +0900

    fix: redis 세션 클러스터링

--- src/main/java/com/hanwha/robotics/user/security/config/SecurityConfig.java
+++ src/main/java/com/hanwha/robotics/user/security/config/SecurityConfig.java
@@ -20,15 +20,12 @@ import com.hanwha.robotics.user.service.impl.MemberLogService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.CookieHttpSessionIdResolver;
-import org.springframework.session.web.http.DefaultCookieSerializer;
-import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-//@EnableRedisHttpSession
+@EnableRedisHttpSession
 public class SecurityConfig {
 
     private final ObjectMapper objectMapper;
@@ -76,24 +73,10 @@ public class SecurityConfig {
                 .and()
                 .addFilterBefore(loginAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(logoutAuthenticationFilter(), LogoutFilter.class)
-
-
         ;
         return http.build();
     }
 
-
-
-    @Bean
-    public HttpSessionIdResolver httpSessionIdResolver() {
-        CookieHttpSessionIdResolver cookieHttpSessionIdResolver = new CookieHttpSessionIdResolver();
-        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-        cookieSerializer.setCookieName("JSESSIONID");
-        cookieSerializer.setCookiePath("/");
-        cookieSerializer.setDomainName("https://hanwharobotics.co.kr:8090");
-        cookieHttpSessionIdResolver.setCookieSerializer(cookieSerializer);
-        return cookieHttpSessionIdResolver;
-    }
 
 
 
