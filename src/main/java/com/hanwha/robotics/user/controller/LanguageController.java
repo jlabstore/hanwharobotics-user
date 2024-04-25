7970413c0e18f893639eeac90d7d01926067b071
hyunjeong <hyunjeong@jlabsoft.com>
2024-04-25 13:13:57 +0900

    chore: 코드정리

--- src/main/java/com/hanwha/robotics/user/controller/LanguageController.java
+++ src/main/java/com/hanwha/robotics/user/controller/LanguageController.java
@@ -1,159 +1,3 @@
-//package com.hanwha.robotics.user.controller;
-//
-//import java.io.IOException;
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import javax.servlet.http.Cookie;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.view.RedirectView;
-//
-//@Controller
-//@RequestMapping("/lang")
-//public class LanguageController {
-//
-//	@Value("${base.url}")
-//	private String baseUrl;
-//
-//	@Value("${base.url.en}")
-//	private String baseUrlEn;
-//
-//	@Value("${cookie.url}")
-//	private String cookieUrl;
-//
-//	@Value("${cookie.url.en}")
-//	private String cookieUrlEn;
-//
-//	@GetMapping("/kr")
-//	public RedirectView kr(
-//		HttpServletRequest request,
-//		HttpServletResponse response,
-//		@RequestParam String path
-//	) throws IOException {
-//		Arrays.stream(request.getCookies())
-//			.filter(cookie -> cookie.getName().equals("JSESSIONID"))
-//			.findFirst()
-//			.ifPresent(jSessionId -> {
-//				Cookie cookie = new Cookie(jSessionId.getName(), jSessionId.getValue());
-//				cookie.setMaxAge(jSessionId.getMaxAge());
-//				cookie.setSecure(jSessionId.getSecure());
-//				cookie.setHttpOnly(jSessionId.isHttpOnly());
-//				cookie.setVersion(jSessionId.getVersion());
-//				cookie.setDomain(cookieUrl);
-//				response.addCookie(cookie);
-//
-//				response.setHeader("Set-Cookie", "JSESSIONID=" + jSessionId.getValue() + "; SameSite=None; Secure");
-//			});
-//
-////		response.sendRedirect(baseUrl + path);
-//		RedirectView redirectView = new RedirectView(baseUrl+path);
-//		redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-//		return redirectView;
-//	}
-//
-////	RedirectView
-//
-//	@GetMapping("/en")
-//	public void en(
-//		HttpServletRequest request,
-//		HttpServletResponse response,
-//		@RequestParam String path
-//	) throws IOException {
-//		Arrays.stream(request.getCookies())
-//			.filter(cookie -> cookie.getName().equals("JSESSIONID"))
-//			.findFirst()
-//			.ifPresent(jSessionId -> {
-//				Cookie cookie = new Cookie(jSessionId.getName(), jSessionId.getValue());
-//				cookie.setMaxAge(jSessionId.getMaxAge());
-//				cookie.setSecure(jSessionId.getSecure());
-//				cookie.setHttpOnly(jSessionId.isHttpOnly());
-//				cookie.setVersion(jSessionId.getVersion());
-//				cookie.setDomain(cookieUrlEn);
-//				response.addCookie(cookie);
-//
-//				response.setHeader("Set-Cookie", "JSESSIONID=" + jSessionId.getValue() + "; SameSite=None; Secure");
-//
-//			});
-//		response.sendRedirect(baseUrlEn + path);
-//	}
-//
-//
-//
-//
-//
-////	@GetMapping("/kr")
-////	public ResponseEntity<Map<String ,String>> kr(
-////			HttpServletRequest request,
-////			HttpServletResponse response,
-////			@RequestParam String path
-////	) throws IOException {
-////		Arrays.stream(request.getCookies())
-////				.filter(cookie -> cookie.getName().equals("JSESSIONID"))
-////				.findFirst()
-////				.ifPresent(jSessionId -> {
-////					Cookie cookie = new Cookie(jSessionId.getName(), jSessionId.getValue());
-////
-////					cookie.setValue(jSessionId.getValue());
-////
-////					cookie.setMaxAge(jSessionId.getMaxAge());
-////					cookie.setSecure(jSessionId.getSecure());
-////					cookie.setHttpOnly(jSessionId.isHttpOnly());
-////					cookie.setVersion(jSessionId.getVersion());
-////					cookie.setDomain(cookieUrl);
-////					response.addCookie(cookie);
-//////                    response.setHeader("Set-Cookie", "JSESSIONID=" + jSessionId.getValue() + "; SameSite=None; none");
-////				});
-//////		response.sendRedirect(baseUrl + path);
-////        Map<String, String> responseMap = new HashMap<>();
-////		responseMap.put("redirectUrl", baseUrl + path);
-////		return ResponseEntity.ok(responseMap);
-////	}
-////
-////	@GetMapping("/en")
-////	public ResponseEntity<Map<String,String>> en(
-////			HttpServletRequest request,
-////			HttpServletResponse response,
-////			@RequestParam String path
-////	) throws IOException {
-////		Arrays.stream(request.getCookies())
-////				.filter(cookie -> cookie.getName().equals("JSESSIONID"))
-////				.findFirst()
-////				.ifPresent(jSessionId -> {
-////					Cookie cookie = new Cookie(jSessionId.getName(), jSessionId.getValue());
-////
-////					cookie.setValue(jSessionId.getValue());
-////
-////					cookie.setMaxAge(jSessionId.getMaxAge());
-////					cookie.setSecure(jSessionId.getSecure());
-////					cookie.setHttpOnly(jSessionId.isHttpOnly());
-////					cookie.setVersion(jSessionId.getVersion());
-////					cookie.setDomain(cookieUrlEn);
-////					response.addCookie(cookie);
-////
-//////					response.setHeader("Set-Cookie", "JSESSIONID=" + jSessionId.getValue() + "; SameSite=None; none");
-////
-////				});
-//////		response.sendRedirect(baseUrlEn + path);
-////        Map<String, String> responseMap = new HashMap<>();
-////		responseMap.put("redirectUrl", baseUrlEn + path);
-////		return ResponseEntity.ok(responseMap);
-////	}
-//
-//
-//}
-
-
-
 package com.hanwha.robotics.user.controller;
 
 import java.io.IOException;
@@ -211,6 +55,7 @@ public class LanguageController {
 	}
 
 
+	// 리다이렉트
 //	@GetMapping("/kr")
 //	public void kr(
 //			HttpServletRequest request,
@@ -263,6 +108,7 @@ public class LanguageController {
 
 
 
+	// url
 //	@GetMapping("/kr")
 //	public ResponseEntity<Map<String ,String>> kr(
 //			HttpServletRequest request,
@@ -312,65 +158,9 @@ public class LanguageController {
 //		responseMap.put("redirectUrl", baseUrlEn + path);
 //		return ResponseEntity.ok(responseMap);
 //	}
-
-
 
 
 	// TODO: path 를 파라미터로 보내고 / 도메인 바뀐상태로 들어왔을떄 쿠키에 set
-//	@GetMapping("/kr")
-//	public ResponseEntity<Map<String ,String>> kr(
-//			HttpServletRequest request,
-//			HttpServletResponse response,
-//			@RequestParam String path
-//	) throws IOException {
-//		Arrays.stream(request.getCookies())
-//				.filter(cookie -> cookie.getName().equals("JSESSIONID"))
-//				.findFirst()
-//				.ifPresent(jSessionId -> {
-//					Cookie cookie = new Cookie(jSessionId.getName(), jSessionId.getValue());
-//					cookie.setValue(jSessionId.getValue());
-//					cookie.setMaxAge(jSessionId.getMaxAge());
-//					cookie.setSecure(jSessionId.getSecure());
-//					cookie.setHttpOnly(jSessionId.isHttpOnly());
-//					cookie.setVersion(jSessionId.getVersion());
-//					cookie.setDomain(cookieUrlEn);
-//					response.addCookie(cookie);
-//					response.setHeader("Set-Cookie", "JSESSIONID=" + jSessionId.getValue() + "; SameSite=None; none");
-//				});
-//        Map<String, String> responseMap = new HashMap<>();
-//		responseMap.put("redirectUrl", baseUrl + path);
-//		return ResponseEntity.ok(responseMap);
-//	}
-//
-//	@GetMapping("/en")
-//	public ResponseEntity<Map<String,String>> en(
-//			HttpServletRequest request,
-//			HttpServletResponse response,
-//			@RequestParam String path
-//	) throws IOException {
-//		Arrays.stream(request.getCookies())
-//				.filter(cookie -> cookie.getName().equals("JSESSIONID"))
-//				.findFirst()
-//				.ifPresent(jSessionId -> {
-//					Cookie cookie = new Cookie(jSessionId.getName(), jSessionId.getValue());
-//					cookie.setValue(jSessionId.getValue());
-//					cookie.setMaxAge(jSessionId.getMaxAge());
-//					cookie.setSecure(jSessionId.getSecure());
-//					cookie.setHttpOnly(jSessionId.isHttpOnly());
-//					cookie.setVersion(jSessionId.getVersion());
-//					cookie.setDomain(cookieUrl);
-//					response.addCookie(cookie);
-//					response.setHeader("Set-Cookie", "JSESSIONID=" + jSessionId.getValue() + "; SameSite=None; none");
-//				});
-//		Map<String, String> responseMap = new HashMap<>();
-//		responseMap.put("redirectUrl", baseUrlEn + path);
-//		return ResponseEntity.ok(responseMap);
-//	}
-
-
-
-
-
 
 
 	@GetMapping("/kr")
@@ -434,15 +224,6 @@ public class LanguageController {
 					response.addCookie(newCookie);
 				});
 	}
-
-
-
-
-
-
-
-
-
 
 
 }
