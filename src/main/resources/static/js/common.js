eaf27c9e900eb7a78a13ec6d59f2cfc49c98c51a
leejuhee <dljuhee@naver.com>
2024-05-17 10:11:52 +0900

    fix: common.js 에코 시스템 슬라이더 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -525,6 +525,77 @@ $('.products-detail-video-slider').slick({
   ]
 })
 
+$('.ecosystem-main-slider1').slick({
+  dots: true,
+  arrows: true,
+  infinite: false,
+  autoplay: false,
+  slidesPerRow: 4,
+  rows: 3,
+  prevArrow: "<button type='button' class='slick-prev'><img src='../images/pc/robots_slider_arrow_left.svg' alt='' /></button>",
+  nextArrow: "<button type='button' class='slick-next'><img src='../images/pc/robots_slider_arrow_right.svg' alt='' /></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesPerRow: 3,
+        rows: 4,
+      }
+    },
+    {
+      breakpoint: 765,
+      settings: {
+        slidesPerRow: 2,
+        rows: 3,
+      }
+    },
+  ]
+})
+
+$('.ecosystem-template-01-slider1').slick({
+  dots: true,
+  arrows: true,
+  infinite: false,
+  autoplay: false,
+  slidesPerRow: 4,
+  rows: 2,
+  prevArrow: "<button type='button' class='slick-prev'><img src='../images/pc/robots_slider_arrow_left.svg' alt='' /></button>",
+  nextArrow: "<button type='button' class='slick-next'><img src='../images/pc/robots_slider_arrow_right.svg' alt='' /></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesPerRow: 3,
+        rows: 2,
+      }
+    },
+    {
+      breakpoint: 765,
+      settings: {
+        slidesPerRow: 2,
+        rows: 3,
+      }
+    },
+  ]
+})
+
+$('.ecosystem-template-01-info-slider').slick({
+  arrows: true,
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  prevArrow: "<button type='button' class='slick-prev'><img src='../images/pc/ecosystem_template_slider_left.svg' alt='' /></button>",
+  nextArrow: "<button type='button' class='slick-next'><img src='../images/pc/ecosystem_template_slider_right.svg' alt='' /></button>",
+})
+
+if (win.outerWidth() <= 765) {
+  $('.tab-nav-mobile').change(function() {
+    let value = $('.tab-nav-mobile option:selected').val();
+    $('.nav-content').hide();
+    $(`#${value}-content`).show();
+  });
+}
+
 if (win.outerWidth() > 765) {
   $('#nav1-1-content').show();
 } else {
