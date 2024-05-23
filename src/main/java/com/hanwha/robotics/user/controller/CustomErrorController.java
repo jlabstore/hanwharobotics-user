c9b797724298fdc76180cd3e98eaf30b12c07e03
hyunjeong <hyunjeong@jlabsoft.com>
2024-05-23 10:02:16 +0900

    fix: 에러 리다이렉트 적용

--- src/main/java/com/hanwha/robotics/user/controller/CustomErrorController.java
+++ src/main/java/com/hanwha/robotics/user/controller/CustomErrorController.java
@@ -1,19 +1,24 @@
 package com.hanwha.robotics.user.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CustomErrorController implements ErrorController {
     
-    // @RequestMapping("/error")
-    // public String handleError(HttpServletRequest request)  {
-    //     Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+     @RequestMapping("/error")
+     public String handleError(HttpServletRequest request)  {
+         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-    //     if(status != null && Integer.valueOf(status.toString()) ==  HttpStatus.NOT_FOUND.value()){
-    //         return "redirect:/";
-    //     }
+         if(status != null && Integer.valueOf(status.toString()) ==  HttpStatus.NOT_FOUND.value()){
+             return "redirect:/";
+         }
 
-    //     return "error/error";
-    // }
+         return "common/error";
+     }
 }
