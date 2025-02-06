d0e38631c0fa70dd6c7c3e45cbb6fba01679562e
hyunjeong <hyunjeong@jlabsoft.com>
2025-02-06 09:10:57 +0900

    feat: 이동로봇 적용사례 리스트 페이징으로 처리

--- src/main/java/com/hanwha/robotics/user/controller/AgvAmrController.java
+++ src/main/java/com/hanwha/robotics/user/controller/AgvAmrController.java
@@ -49,16 +49,33 @@ public class AgvAmrController {
         return "agv_amr/case_studies_list";
     }
 
+//    @PostMapping("/case-studies")
+//    public ResponseEntity<Object> findCaseByCategory(
+//        RobotRequest robotRequest,
+//        HttpServletRequest request
+//    ) {
+//        String lang = commonUtil.getCookieLang(request);
+//        robotRequest.setLang(lang);
+//        List<RobotResponse> robotResponseList = robotService.findRobotByCategory(robotRequest);
+//        return ResponseEntity.ok(ApiResponse.res(ApiStatus.OK.getValue(), ApiStatus.OK.name(), robotResponseList));
+//    }
+
+
+
     @PostMapping("/case-studies")
     public ResponseEntity<Object> findCaseByCategory(
         RobotRequest robotRequest,
+        PageRequest pageRequest,
         HttpServletRequest request
     ) {
         String lang = commonUtil.getCookieLang(request);
         robotRequest.setLang(lang);
-        List<RobotResponse> robotResponseList = robotService.findRobotByCategory(robotRequest);
-        return ResponseEntity.ok(ApiResponse.res(ApiStatus.OK.getValue(), ApiStatus.OK.name(), robotResponseList));
+//    List<RobotResponse> robotResponseList = robotService.findRobotByCategory(robotRequest);
+        PageResponse body = robotService.findRobotByCategory2(robotRequest, pageRequest, lang);
+        return ResponseEntity.ok(ApiResponse.res(ApiStatus.OK.getValue(), ApiStatus.OK.name(), body));
     }
+
+
 
     @GetMapping("/case-studies/{robotNo}")
     public String caseStudiesViewPage(@PathVariable int robotNo, Model model) {
